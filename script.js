e16700d01625cede771486ae7125d5c512c54558
edo979 <prezentabilan@gmail.com>
2022-06-01 15:05:13 +0200

    cleaning

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 const chartBarEls = document.querySelectorAll('.chart_bar')
 
-//console.log(chartBarEls)
-// chartBarEls.forEach((barEl) => {
-//   console.log(barEl)
-// })
-
 showData()
-
-async function getData() {
-  const res = await fetch('data.json')
-  return await res.json()
-}
 
 async function showData() {
   const data = await getData()
@@ -18,12 +8,15 @@ async function showData() {
     prev.amount > curr.amount ? prev : curr
   ).amount
 
-  console.log(maxAmount)
-
   data.forEach((day, i) => {
     const { day: dayName, amount } = day,
-      amountPercentage = (100 * amount) / maxAmount
+      amountPercentage = (amount / maxAmount) * 100
 
     chartBarEls[i].firstElementChild.style.height = `${amountPercentage}%`
   })
+}
+
+async function getData() {
+  const res = await fetch('data.json')
+  return await res.json()
 }
