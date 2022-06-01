7ddb8decc0b084b8c2f2a5a97d90ddcf58e6aa5e
edo979 <prezentabilan@gmail.com>
2022-06-01 19:01:30 +0200

    set --content, finish js

--- script.js
+++ script.js
@@ -4,18 +4,22 @@ showData()
 
 async function showData() {
   const data = await getData(),
-    maxAmount = data.reduce((prev, curr) =>
-      prev.amount > curr.amount ? prev : curr
-    ).amount,
+    //  for el max height
+    maxAmount = getMaxAmountValue(data),
     currentDay = new Date().getDay()
 
   data.forEach((day, i) => {
+    // a bar in the chart
     const amountEl = chartBarEls[i].firstElementChild,
-      { amount } = day,
+      // amount value from data object
+      amount = day.amount,
       amountPercentage = (amount / maxAmount) * 100
 
+    // style a bar
     amountEl.style.height = `${amountPercentage}%`
+    amountEl.style.setProperty('--content', `'$${amount}'`)
 
+    // change color of current day
     if (i + 1 == currentDay) {
       amountEl.classList.add('accent')
     }
@@ -25,4 +29,9 @@ async function showData() {
 async function getData() {
   const res = await fetch('data.json')
   return await res.json()
+}
+
+function getMaxAmountValue(data) {
+  return data.reduce((prev, curr) => (prev.amount > curr.amount ? prev : curr))
+    .amount
 }
