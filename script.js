41d24d197ece08343d1ddda2435781cfa3ca174b
edo979 <prezentabilan@gmail.com>
2022-06-01 18:11:32 +0200

    add current day accent color

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ const chartBarEls = document.querySelectorAll('.chart_bar')
 showData()
 
 async function showData() {
-  const data = await getData()
-  const maxAmount = data.reduce((prev, curr) =>
-    prev.amount > curr.amount ? prev : curr
-  ).amount
+  const data = await getData(),
+    maxAmount = data.reduce((prev, curr) =>
+      prev.amount > curr.amount ? prev : curr
+    ).amount,
+    currentDay = new Date().getDay()
 
   data.forEach((day, i) => {
-    const { day: dayName, amount } = day,
+    const amountEl = chartBarEls[i].firstElementChild,
+      { amount } = day,
       amountPercentage = (amount / maxAmount) * 100
 
-    chartBarEls[i].firstElementChild.style.height = `${amountPercentage}%`
+    amountEl.style.height = `${amountPercentage}%`
+
+    if (i + 1 == currentDay) {
+      amountEl.classList.add('accent')
+    }
   })
 }
 
